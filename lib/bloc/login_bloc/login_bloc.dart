354195acb9338454d6c6da12894bbe0e4eac65d2
nodir <nodirbarotov0707@gmail.com>
2021-10-02 12:05:39 +0500

    Last commit

--- lib/bloc/login_bloc/login_bloc.dart
+++ lib/bloc/login_bloc/login_bloc.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:global_news/bloc/login_bloc/login_event.dart';
 import 'package:global_news/bloc/login_bloc/login_state.dart';
 import 'package:global_news/repositories/user_repositories.dart';
+import 'package:global_news/utils/validators.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final UserRepository _userRepository;
@@ -10,8 +11,32 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
 
   @override
   Stream<LoginState> mapEventToState(LoginEvent event) async* {
-    // if (event is LoginEmailChange) {
-    //   yield 
-    // }
+    if (event is LoginEmailChange) {
+      yield* _mapLoginEmailChangeToState(event.email);
+    } else if (event is LoginPasswordChange) {
+      yield* _mapLoginPasswordChangeToState(event.password);
+    } else if (event is LoginWithCredentialsPressed) {
+      yield* _mapLoginWithCredentialPressedToState(
+          email: event.email, password: event.password);
+    }
+  }
+
+  Stream<LoginState> _mapLoginEmailChangeToState(String email) async* {
+    yield state.update(isEmailValid: Validators.isValidEmail(email));
+  }
+
+  Stream<LoginState> _mapLoginPasswordChangeToState(String password) async* {
+    yield state.update(isPasswordValid: Validators.isValidPassword(password));
+  }
+
+  Stream<LoginState> _mapLoginWithCredentialPressedToState(
+      {String email, String password}) async* {
+    yield LoginState.loading();
+    try {
+      await _userRepository.signInWithCredentials(email, password);
+      yield LoginState.success();
+    } catch (_) {
+      yield LoginState.failure();
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:global_news/bloc/auth.bloc/auth_bloc.dart';
+import 'package:global_news/bloc/simple_bloc_observer.dart';
+import 'package:global_news/repositories/user_repositories.dart';
 import 'package:global_news/screens/main_screens/welcoming_screen.dart';
 
 import 'screens/main_screens/welcoming_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  Bloc.observer = SimpleBlocObserver();
+  final UserRepository userRepository = UserRepository();
+  runApp(
+    BlocProvider(
+      create: (context) => AuthenticationBloc(userRepository: userRepository),
+      child: MyApp(
+        userRepository: userRepository,
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  final UserRepository _userRepository;
+  MyApp({UserRepository userRepository}) : _userRepository = userRepository;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
